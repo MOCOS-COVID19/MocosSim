bc8a85a6be707076938c299956aa190c0691ca48
Marcin Bodych <bodychmarcin@gmail.com>
2020-02-18 07:20:31 +0100

    first test case added

--- src/models/infection_model.py
+++ src/models/infection_model.py
@@ -6,15 +6,16 @@ import collections
 import enum
 import json
 import os
+import typing
 
 import numpy as np
 import pandas as pd
-from schema import (Schema, And, Use)
+from schema import (Schema, And, Use, Or)
 
 # Todo: below should go to separate file
 
 
-class DiseaseState(enum.Enum):
+class InfectionStatus(enum.Enum):
     Healthy = -1
     Contraction = 0
     Infectious = 1
@@ -22,10 +23,16 @@ class DiseaseState(enum.Enum):
     RequiringHospital = 3
     Terminated = 999
 
+
 # https://stackoverflow.com/questions/24481852/serialising-an-enum-member-to-json
 
+
+CONTRACTION_TIME = 'contraction_time'
+PERSON_INDEX = 'person_index'
+INFECTION_STATUS = 'infection_status'
+INITIAL_CONDITIONS = 'initial_conditions'
 PUBLIC_ENUMS = {
-    'DiseaseState': DiseaseState,
+    'InfectionStatus': InfectionStatus,
 }
 
 class EnumEncoder(json.JSONEncoder):
@@ -42,41 +49,56 @@ def as_enum(d):
     else:
         return d
 
-CONTRACTION_TIME = 'contraction_time'
-PERSON_INDEX = 'person_index'
-DISEASE_STATE = 'disease_state'
-INITIAL_CONDITIONS = 'initial_conditions'
 
-initial_conditions_schema = Schema([{PERSON_INDEX: int, CONTRACTION_TIME: float,
-                                     DISEASE_STATE: [DiseaseState.Healthy, DiseaseState.Contraction,
-                                                     DiseaseState.Infectious, DiseaseState.StayingHome,
-                                                     DiseaseState.RequiringHospital, DiseaseState.Terminated]
+initial_conditions_schema = Schema([{PERSON_INDEX: int, CONTRACTION_TIME: Or(float, int),
+                                     INFECTION_STATUS: Or(InfectionStatus.Healthy, InfectionStatus.Contraction,
+                                                          InfectionStatus.Infectious, InfectionStatus.StayingHome,
+                                                          InfectionStatus.RequiringHospital, InfectionStatus.Terminated)
                                      }])
-active_states = [DiseaseState.Contraction, DiseaseState.Infectious,
-                 DiseaseState.StayingHome, DiseaseState.RequiringHospital]
-default_initial_conditions = ([{PERSON_INDEX: 0, CONTRACTION_TIME: 0, DISEASE_STATE: DiseaseState.Contraction}])
+active_states = [InfectionStatus.Contraction, InfectionStatus.Infectious,
+                 InfectionStatus.StayingHome, InfectionStatus.RequiringHospital]
+default_initial_conditions = ([{PERSON_INDEX: 0, CONTRACTION_TIME: 0, INFECTION_STATUS: InfectionStatus.Contraction}])
 default_stop_simulation_threshold = 10000
 stop_simulation_threshold_schema = Schema(And(Use(int), lambda x: x > 0))
 
 
+class EventQueue:
+    pass
+
+
 class InfectionModel:
-    def __init__(self, params_path, df_individuals_path):
-        params = json.loads(params_path)  # TODO: check whether this should be moved to different place
+    def __init__(self, params_path: str, df_individuals_path: str) -> None:
+        with open(params_path, 'r') as params_file:
+            params = json.loads(params_file.read(),
+                                object_hook=as_enum)  # TODO: check whether this should be moved to different place
+
         self._initial_conditions = initial_conditions_schema.validate(
             params.get(INITIAL_CONDITIONS, default_initial_conditions)
         )
         self._global_time = 0.0
         self._stop_simulation_threshold = params.get('stop_simulation_threshold', default_stop_simulation_threshold)
-        self._df_individuals = pd.read_csv(df_individuals_path, headers=True)
-        self._df_individuals[DISEASE_STATE] = DiseaseState.Healthy
+        self._df_individuals = pd.read_csv(df_individuals_path)  # TODO: Consider networkx graph instead of pandas df
         self._df_individuals[CONTRACTION_TIME] = np.nan
         for initial_condition in self._initial_conditions:
-            self._df_individuals.loc[initial_condition.person_index,
-                                     DISEASE_STATE] = initial_condition.disease_state
-            self._df_individuals.loc[initial_condition.person_index,
-                                     CONTRACTION_TIME] = initial_condition.contraction_time
+            for key in [INFECTION_STATUS, CONTRACTION_TIME]:
+                self._df_individuals.loc[initial_condition[PERSON_INDEX], key] = initial_condition[key]
+
+    @property
+    def global_time(self):
+        return self._global_time
+
+    @property
+    def df_individuals(self):
+        return self._df_individuals
+
+    @property
+    def stop_simulation_threshold(self):
+        return self._stop_simulation_threshold
+
+    def active_people(self):
+        return self._df_individuals[INFECTION_STATUS].value_counts().filter(items=active_states, axis=0).sum()
 
     def run_simulation(self):
-        number_active_people = self._df_individuals[DISEASE_STATE].value_counts().filter(items=active_states, axis=0).sum()
+        number_active_people = self.active_people()
         # TODO
         pass
--- test/models/test_infection_model.py
+++ test/models/test_infection_model.py
@@ -2,7 +2,15 @@ from unittest import TestCase
 from src.models import infection_model
 import numpy as np
 import random
+import os
 
 
 class TestInfectionModel(TestCase):
-    pass
+    def test_init_with_dummy_params(self):
+        dummy_params_path = os.path.join('test', 'models', 'assets', 'dummy_params.json')
+        dummy_individuals_path = os.path.join('test', 'models', 'assets', 'dummy_individuals.csv')
+        model = infection_model.InfectionModel(params_path=dummy_params_path,
+                                               df_individuals_path=dummy_individuals_path)
+        df_individuals = model.df_individuals
+        assert 9 == len(df_individuals)
+        assert 1 == model.active_people()
