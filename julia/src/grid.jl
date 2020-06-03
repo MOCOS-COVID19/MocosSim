70475edc27d0cbae14893460c17e216e4e6624b9
Tomasz Ożański <tomasz.ozanski@pwr.edu.pl>
2020-06-04 01:29:07 +0200

    julia grid creation

---
+++ julia/src/grid.jl
@@ -0,0 +1,83 @@
+using CSV
+using DataFrames
+using DataStructures
+using JSON
+const Path = Vector{String}
+
+function parserange(str::AbstractString)
+  parts = split(str, ":")
+  if 2 == length(parts)
+      b = parse(Float64, parts[1])
+      e = parse(Float64, parts[2])
+      b:e
+  elseif 3 == length(parts)
+      b = parse(Float64, parts[1])
+      s = parse(Float64, parts[2])
+      e = parse(Float64, parts[3])
+      b:s:e
+  else
+      nothing
+  end
+end
+
+function findranges(dict::AbstractDict{String,T} where T)
+  paths = Vector{Path}()
+  for key in keys(dict)
+      val = dict[key]
+      if isa(val, AbstractString)
+          if nothing != parserange(val)
+              push!(paths, Path([key]))
+          end
+      elseif isa(val, AbstractDict)
+          append!(paths, map(path->insert!(copy(path), 1, key), findranges(dict[key])))
+      end
+  end
+  paths
+end
+
+getbypath(dict::AbstractDict{T} where T<:AbstractString, path::Path) = getbypath(dict[path[1]], path[2:end])
+getbypath(val, ::Path) = val
+
+function setbypath!(dict::AbstractDict{T} where T<:AbstractString, path::Path, value::Real) 
+  if length(path) > 1
+      setbypath!(dict[path[1]], path[2:end], value)
+  else
+      dict[only(path)] = value
+  end
+end
+
+function main()
+  @assert length(ARGS) > 0 "JSON file needed"
+
+  json = JSON.parsefile(ARGS[1], dicttype=OrderedDict)
+  workdir = ARGS[2]
+
+  rangepaths = findranges(json) |> sort
+  ranges = map(x->getbypath(json, x) |> parserange, rangepaths)
+
+  @info "ranges are" rangepaths ranges
+
+  @assert length(ranges) == 2 "we support grids now only"
+  xpath, ypath = rangepaths
+  xrange, yrange = ranges
+
+  df = DataFrame([String[], Float64[], Float64[]], ["path", join(xpath, "_"), join(ypath, "_")])
+
+  for (x,xval) in enumerate(xrange)
+    for (y,yval) in enumerate(yrange)
+      subdir = joinpath(workdir, "grid_$(x-1)_$(y-1)")
+      mkpath(subdir)
+      specific_json = deepcopy(json)
+      setbypath!(specific_json, xpath, xval)
+      setbypath!(specific_json, ypath, yval)
+
+      open(joinpath(subdir, "params_experiment.json"),"w") do f
+        JSON.print(f, specific_json)
+      end
+      push!(df, (joinpath(subdir, "output.jld2"), xval, yval))
+    end
+  end
+  CSV.write(joinpath(workdir, "parameters_map.csv"), df)
+end
+
+main()
