b9ebb8c6566d563fed172a2e0f3be35c83aaeb43
Rados�aw Idzikowski <radoslaw.idzikowski@pwr.edu.pl>
2021-08-24 00:49:16 +0200

    clean in travel function

--- src/params/travels.jl
+++ src/params/travels.jl
@@ -7,18 +7,8 @@ Base.@kwdef struct Holiday <: InfectionTravels
   
   function (f::Holiday)(state::SimState, params::SimParams, event::Event, infection_time::Real=0.0)
     @assert kind(event) == OutsideTransmissionEvent
-    ck = contactkind(event)
-    #if ConstantKernelContact !== ck && SporadicContact !== ck && FriendshipContact !== ck
-    #  return true # do not affect other types of contact than "outer" ones
-    #end
-    #rand(state.rng) < max((Float64(-abs(infection_time-f.peak)*abs(infection_time-f.peak)))/1500.0 + f.height,f.minimum)
-    vaule = rand(state.rng)
-    #probability =  max((Float64(-abs(infection_time-f.peak)*abs(infection_time-f.peak)))/1500.0 + f.height,f.minimum)
-    probability =  max((Float64(-abs(event.time-f.peak)*abs(event.time-f.peak)))/1500.0 + f.height,f.minimum)
-    #@info "value" vaule
-    #@info "probability" probability
-    vaule < probability
-    
+    days = (f.days/2)^2
+    rand(state.rng) < max((Float64(-abs(event.time-f.peak)^2)*f.height)/days + f.height, f.minimum)
   end
   
   # This all to avoid using @eval and others
