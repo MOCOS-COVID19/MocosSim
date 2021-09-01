73d58c8b67b0e959726133c3d074aba314fec4ca
Marcin Bodych <bodychmarcin@gmail.com>
2021-09-02 01:47:25 +0200

    added tests

--- test/test_modulations.jl
+++ test/test_modulations.jl
@@ -1,9 +1,13 @@
-include("../src/enums.jl")
-include("../src/event.jl")
+import MocosSim: StrainKind, ContactKind, ConstantKernelContact, NullStrain,
+                 TransmissionEvent, Event, HouseholdContact
+
+mutable struct DummyParams <: MocosSim.AbstractSimParams
+  DummyParams() = new()
+end
 
 @testset "InfectionModulations" begin
   state = MocosSim.SimState(3)
-  params = missing
+  params::DummyParams = DummyParams()
   strain::StrainKind = NullStrain
   contact_kind::ContactKind = ConstantKernelContact
   event = Event(Val(TransmissionEvent), 0.0, 0, 0, contact_kind, strain)
@@ -18,10 +22,13 @@ include("../src/event.jl")
 
     modulation = MocosSim.TanhModulation(weight_detected, weight_deaths, weight_days, loc, scale,
                                          limit_value)
-    state.time = 100
-    @test modulation(state, params, event) == false
-
-    household_event = Event(Val(TransmissionEvent), 0.0, 0, 0, HouseholdContact, strain)
-    @test modulation(state, params, household_event) == true
+    @testset "Limit value 0.0 results in rejecting all events" begin
+      state.time = 100
+      @test modulation(state, params, event) == false
+    end
+    @testset "Household event is not modified" begin
+      household_event = Event(Val(TransmissionEvent), 0.0, 0, 0, HouseholdContact, strain)
+      @test modulation(state, params, household_event) == true
+    end
   end
 end
