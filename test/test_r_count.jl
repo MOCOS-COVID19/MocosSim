0270d319fce3dc6827a2ecead864b81fb20da977
Jacek Skoczylas <gjacek6@gmail.com>
2022-03-22 23:24:56 +0100

    changes in r count

--- test/test_r_count.jl
+++ test/test_r_count.jl
@@ -13,6 +13,7 @@ const TimePoint = Fixed{Int32, 16}
 include("../src/enums.jl")
 include("../src/event.jl")
 include("../src/robin_forest.jl")
+include("../src/r_count.jl")
 
 function trans(; src, subject, t=0.0)
     return Event(Val(TransmissionEvent), time=t, subject=subject, source=src, contact_kind=NoContact, strain=NullStrain)
@@ -31,33 +32,33 @@ function nan2zero(x)
 
         EPS = 0.00001
 
-@testset "RCountTesting" begin
-    @testset "Basic R count test - small 1" begin
-        include("../src/r_count.jl")
-        f = RobinForest(10)
-        push!(f, trans(src=1, subject=2, t=1.1))
 
-        push!(f, trans(src=2, subject=3, t=2.1))
-        push!(f, trans(src=2, subject=4, t=2.2))
-        push!(f, trans(src=2, subject=5, t=2.2))
+function test1()
+    f = RobinForest(10)
+    push!(f, trans(src=1, subject=2, t=1.1))  # day 1
 
-        push!(f, trans(src=5, subject=6, t=3.2))
-        push!(f, trans(src=6, subject=7, t=3.3))
+    push!(f, trans(src=2, subject=3, t=2.1))  # day 2
+    push!(f, trans(src=2, subject=4, t=2.2))  # day 2
+    push!(f, trans(src=2, subject=5, t=2.2))  # day 2
 
-        max_day = maximum(f.inedges) do e
-            time(e) |> floor |> Int
-        end
+    push!(f, trans(src=5, subject=6, t=3.2))  # day 3
+    push!(f, trans(src=6, subject=7, t=3.3))  # day 3
 
-        infected_per_day = count_infected_per_time(f, max_day)
-        println("Number of infected people per each day ($max_day days total) = $infected_per_day")
-        @test infected_per_day == [1, 3, 2]
-
-
-        r_per_time = count_r_per_time(f, max_day)
-        println("R value per each day ($max_day days total) = $r_per_time")
-        @test max_abs_diff(r_per_time, [NaN, 0.75, 0.3333333333333333]) < EPS
+    max_day = maximum(f.inedges) do e
+        time(e) |> floor |> Int
     end
+
+#         infected_per_day = count_infected_per_time(f, max_day)
+#         println("Number of infected people per each day ($max_day days total) = $infected_per_day")
+#         @test infected_per_day == [1, 3, 2]
+
+
+    r_per_time = count_r_per_time(f, max_day)
+    println("R value per each day ($max_day days total) = $r_per_time")
+#     @test max_abs_diff(r_per_time, [1, 3, 0.33, 0.5] ) < EPS
 end
+
+test1()
 
 #=
 On Jacek machine it runs like:
